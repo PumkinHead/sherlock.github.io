9cd7a4cc177dc3752cab1666ac0442cf61b94e4f
SherLock <13258266113@163.com>
2017-08-09 21:00:26 +0800

    spring-server

---
+++ spring-server-master/src/main/java/com/aiesst/AiesstApplication.java
@@ -0,0 +1,12 @@
+package com.aiesst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiesstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiesstApplication.class, args);
+	}
+}
